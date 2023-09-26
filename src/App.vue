8fc2fc3192c6feff40f5aa3b0f9841bf7f8f7ec3
Betty <betty.mj@pitsolutions.com>
2023-09-26 14:57:15 +0530

    Add project files.

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1>Movie Information</h1>
+    <input v-model="searchQuery" placeholder="Search by name or director" />
+    <div class="movies">
+      <Movie
+        v-for="movie in filteredMovies"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue' // Import the Movie component
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      movies: [{'id':1,'director':'1','title':'Titanic'},
+{'id':2,'director':'1','title':'The Terminator'},
+{'id':3,'director':'1','title':'Avatar'},
+{'id':4,'director':'2','title':'Baahubali'},
+{'id':5,'director':'2','title':'RRR'},
+{'id':6,'director':'2','title':'Avatar'},
+{'id':7,'director':'3','title':'Guardians Of Galaxy'},
+{'id':8,'director':'4','title':'The Dark Knight'},
+{'id':9,'director':'4','title':'Inception'},
+{'id':10,'director':'4','title':'Batman Begins'}],
+      searchQuery: ''
+    }
+  },
+  computed: {
+    filteredMovies () {
+      const query = this.searchQuery.toLowerCase()
+      return this.movies.filter(
+        (movie) =>
+          movie.name.toLowerCase().includes(query) ||
+          movie.director.toLowerCase().includes(query)
+      )
+    }
+  }
+}
+</script>
+
+<style>
+.movies {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="movie">
+    <h2>{{ movie.name }}</h2>
+    <p>Directed By: {{ movie.director }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.movie {
+  margin: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+}
+</style>
