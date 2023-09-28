bec599598e8b3f5ed726cfff9250fdf509801bc7
Betty <betty.mj@pitsolutions.com>
2023-09-28 14:30:39 +0530

    Data displayed

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,39 @@
 <template>
-  <div id="app">
+  <div class="app">
     <h1>Movie Information</h1>
     <input v-model="searchQuery" placeholder="Search by name or director" />
-    <div class="movies">
-      <Movie
+    <div class="movies" text-align = "left">
+      <HelloWorld
         v-for="movie in filteredMovies"
         :key="movie.id"
         :movie="movie"
       />
+      <div>
+        <ol>
+          <li v-for="(movie, index) in filteredMovies" :key="index">{{ movie.title }} - Directed by {{ movie.director }}</li>
+        </ol>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue' // Import the Movie component
+// import HelloWorld from './components/HelloWorld.vue' // Import the Movie component
 
 export default {
-  components: {
-    HelloWorld
-  },
   data () {
     return {
-      movies: [{'id':1,'director':'1','title':'Titanic'},
-{'id':2,'director':'1','title':'The Terminator'},
-{'id':3,'director':'1','title':'Avatar'},
-{'id':4,'director':'2','title':'Baahubali'},
-{'id':5,'director':'2','title':'RRR'},
-{'id':6,'director':'2','title':'Avatar'},
-{'id':7,'director':'3','title':'Guardians Of Galaxy'},
-{'id':8,'director':'4','title':'The Dark Knight'},
-{'id':9,'director':'4','title':'Inception'},
-{'id':10,'director':'4','title':'Batman Begins'}],
+      movies: [
+        {'id': 1, 'director': 'James Cameroon', 'title': 'Titanic'},
+        {'id': 2, 'director': 'James Cameroon', 'title': 'The Terminator'},
+        {'id': 3, 'director': 'James Cameroon', 'title': 'Avatar'},
+        {'id': 4, 'director': 'Rajamouli', 'title': 'Baahubali'},
+        {'id': 5, 'director': 'Rajamouli', 'title': 'RRR'},
+        {'id': 6, 'director': 'Rajamouli', 'title': 'Avatar'},
+        {'id': 7, 'director': 'Raman H S', 'title': 'Guardians Of Galaxy'},
+        {'id': 9, 'director': 'Christopher Nolan', 'title': 'Inception'},
+        {'id': 10, 'director': 'Christopher Nolan', 'title': 'Batman Begins'}
+      ],
       searchQuery: ''
     }
   },
@@ -39,7 +42,7 @@ export default {
       const query = this.searchQuery.toLowerCase()
       return this.movies.filter(
         (movie) =>
-          movie.name.toLowerCase().includes(query) ||
+          movie.title.toLowerCase().includes(query) ||
           movie.director.toLowerCase().includes(query)
       )
     }
@@ -51,5 +54,13 @@ export default {
 .movies {
   display: flex;
   flex-wrap: wrap;
+  align-content: center;
+}
+.app{
+  background-color: aqua;
+  align-content: center;
+  text-align: center; /* Center the text horizontally */
+  /*max-width: 600px; /* Adjust this value as needed */
+  /*padding: 20px;*/
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="movie">
-    <h2>{{ movie.name }}</h2>
+    <h2>{{ movie.title }}</h2>
     <p>Directed By: {{ movie.director }}</p>
   </div>
 </template>
